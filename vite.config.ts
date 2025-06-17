ae4ca09e570d0806686be3dfe36f9f3135aadf42
Emanuel <emanuel.sg@proton.me>
2025-06-16 21:14:53 -0300

    arrumando app para evitar erro

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.ts',
-            ssr: 'resources/js/ssr.ts',
+            input: ['resources/js/app.ts'],
             refresh: true,
         }),
         vue({
